c1fa474918adf13be6c9bd0ff1f14c2cd4372280
Migster351 <migscord@gmail.com>
2025-05-24 11:01:01 -0300

    Adiciona WeasyPrint e suas dependências

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 # exit on error
 set -o errexit
 
-# Instala wkhtmltopdf
+# Instala dependências do sistema
 apt-get update
 apt-get install -y wkhtmltopdf
+apt-get install -y python3-cffi python3-brotli libpango-1.0-0 libharfbuzz0b libpangoft2-1.0-0 libffi-dev libjpeg-dev libopenjp2-7-dev
 
 # Instala as dependências Python
 pip install -r requirements.txt 
